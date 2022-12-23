858b0160509cfd0fbda2fdf958de19d351150cfa
mkruszyniak <maciejkruszyniak@gmail.com>
2022-12-23 15:38:44 +0100

    eslint & prettier & editorconfig config

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,6 @@
 import { Application } from 'express';
 
 export default (app: Application) => {
-	const routes = () => {
-  };
-	routes();
+  const routes = () => {};
+  routes();
 };
--- backend/src/setupDatabase.ts
+++ backend/src/setupDatabase.ts
@@ -1,24 +1,23 @@
 import mongoose from 'mongoose';
-import colors from 'colors';
 import { config } from './config';
 import Logger from 'bunyan';
 
 const log: Logger = config.createLogger('setupDatabase');
 
 export default () => {
-	const connect = () => {
-		mongoose
-			// .connect(`mongodb://127.0.0.1:27017/messengerApp-backend`)
-			.connect(`${config.DATABASE_URL}`)
-			.then(() => {
-				log.info('Successfully connected to database.'.bgWhite.magenta);
-			})
-			.catch(error => {
-				log.error('-------- Error connecting to database --------'.red, error);
-				return process.exit(1);
-			});
-	};
-	connect();
+  const connect = () => {
+    mongoose
+      // .connect(`mongodb://127.0.0.1:27017/messengerApp-backend`)
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Successfully connected to database.');
+      })
+      .catch((error) => {
+        log.error('-------- Error connecting to database --------', error);
+        return process.exit(1);
+      });
+  };
+  connect();
 
-	mongoose.connection.on('disconnected', connect);
+  mongoose.connection.on('disconnected', connect);
 };
